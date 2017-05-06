6a64808f92917ea7509b6bc6d6fa591af3e67117
jonelmawirat <jonel.mawirat@bugomi.com>
2017-05-06 04:08:55 +0000

    network prog

--- main.py
+++ main.py
@@ -1,13 +1,52 @@
-#this is my new commit
+#var1 = "ronnel"
+#print(var1)
 
-dasdad
+#var2 = 1+1
+#var3= 2+5
+#var4 = var2+ var3
+#print(var1,var2,var3,var4)
+#_________________________________
 
-sample1
+#hostname1 ="192.168.1.1"
+#hostname2="172.16.1.1"
+#hostname3="98.122.7.7"
+#print("the hostname is:{}".format(hostname))
+#print("the host name is{} \n hostname2{}\n hostname3{}".format(hostname1,hostname2,hostname3))
+#_________________________________
+#import os
 
-two
+#os.system("ping -c 1 8.8.8.8")
 
-three
 
-four
+#response=os.system("ping -c 1 192.1.1.1")
+#print (response)
 
-five
+#_____________________
+
+#var = 0
+#if var == 0:
+ #   print ("var is qual to zero")
+  #  print("hello")
+   # print("hellow245")
+#elif var == 1:
+ #   print("var is equal to 1")
+#elif var ==2:
+ #   print("var2"):
+#else:
+ #   print("var is unknown")
+ 
+ 
+ 
+import os
+#os.system("ping -c 1 8.8.8.8")
+#os.system("ping -c 1 8.8.8.4")
+
+
+
+#response=os.system("ping -c 1 192.1.1.1")
+
+#print (response)
+
+
+response=os.system("ping -c 1 8.8.8.8")
+print (response)
